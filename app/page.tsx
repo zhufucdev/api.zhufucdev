c05ca7126f26048c627dbb62b5b934b5ce764b08
zhufucdev <1095291434@qq.com>
2023-07-23 22:18:41 +0800

    Bug: escape char

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
     return (
         <main>
-            <h1> Welcome to Steve's API! </h1>
-            <p> If you have no idea what are you doing, just let go </p>
+            <h1>Welcome to Steve&apos;s API!</h1>
+            <p>If you have no idea what are you doing, just let go</p>
         </main>
     );
 }
