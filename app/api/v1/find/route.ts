e4c928769d1630e593d4d1c808ce5114575affdf
zhufucdev <1095291434@qq.com>
2023-07-24 14:35:13 +0800

    Rewrite the find API

--- app/api/v1/find/route.ts
+++ app/api/v1/find/route.ts
@@ -1,50 +1,34 @@
-import {Release, handleRelease, ProductProfile} from "@/lib/common";
 import {getAll} from "@vercel/edge-config";
 import {NextRequest, NextResponse} from "next/server";
+import {ProductProfile} from "@/lib/common";
 
 export const runtime = "edge";
 
+interface ProductQuery {
+    name: string;
+    key: string;
+}
+
 export async function GET(req: NextRequest) {
     const params = req.nextUrl.searchParams;
-    const key = params.get("category"),
-        os = params.get("os"),
-        arch = params.get("arch"),
-        current = params.get("current");
-
-    if (!key) {
+    if (!params.has('category')) {
         return NextResponse.json("category is not optional", {status: 400});
     }
-
-    const filters = key.split(',');
-    const products = (await getAll()) as ProductProfile[];
-    let targets: string[] = [];
-    for (const product in products) {
-        const category = products[product].category;
-        if (!Array.isArray(category)) {
-            continue;
-        }
+    const categories = params.get('category')!.split(',');
+    const queried: ProductQuery[] = [];
+    const available = (await getAll()) as ProductProfile[];
+    for (const key in available) {
+        const product = available[key];
         let content = true;
-        for (const filter of filters) {
-            if (!category.includes(filter)) {
+        for (const tag of categories) {
+            if (product.category?.includes(tag) !== true) {
                 content = false;
                 break;
             }
         }
         if (content) {
-            targets.push(product);
+            queried.push({name: product.name, key})
         }
     }
-    const results = await Promise.all(
-        targets.map((p) =>
-            handleRelease(req, p, os, arch, current).then((v) =>
-                v.ok ? v.json() : undefined,
-            ),
-        ),
-    );
-    const responseBody: Release[] = [];
-    for (const result of results) {
-        if (result) responseBody.push(result);
-    }
-
-    return NextResponse.json(responseBody);
+    return NextResponse.json(queried)
 }
